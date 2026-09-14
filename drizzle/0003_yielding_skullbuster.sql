351b415b32f3667456284b03d0927e189ac6e1cb
kucho926 <seungwonyun683@gmail.com>
2026-09-14 20:27:55 +0900

    Add worksheet storage migration

---
+++ drizzle/0003_yielding_skullbuster.sql
@@ -0,0 +1,3 @@
+ALTER TABLE `assignments` ADD `worksheet_key` text;--> statement-breakpoint
+ALTER TABLE `assignments` ADD `worksheet_name` text;--> statement-breakpoint
+ALTER TABLE `assignments` ADD `worksheet_type` text;
